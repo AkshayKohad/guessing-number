dfed0b2286eb94a155fc7ef5e5722efffe34ecff
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2020-12-12 12:55:26 +0530

    Update guess.js

--- guess.js
+++ guess.js
@@ -15,23 +15,39 @@ while(!RangeNumber)
 const DecidedNumber= Math.floor(Math.random()*RangeNumber)+1
 
 var decider=false
-
-while(!decider)
+var quitter=false
+var attempt=1;
+while(!decider && !quitter)
 {
+
 	let InputNumber=prompt("Guess the number")
+
 	if(InputNumber>DecidedNumber)
 	{
 		alert("Input Number is high from correct Guess")
+		attempt++
 	}
 
 	else if(InputNumber<DecidedNumber)
 	{
 		alert("Input Number is low from correct Guess")
+        attempt++
 	}
 
 	else
 	{
 		alert("CONGO YOU GUESSED CORRECT NUMBER")
 		decider=true
+		break
 	}
+var quit=parseInt(prompt("tired of playing want to quit this guessing press 1"))
+if(quit==1)
+	quitter=true
+
+}
+if(decider===true)
+alert(`YOU GOT IN ${attempt} attempts`)
+
+else{
+	alert("Better Luck next time")
 }
